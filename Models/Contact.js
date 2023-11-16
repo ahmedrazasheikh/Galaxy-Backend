cc4fce9a8b8fff13b56532b822852daef21c88e1
Aisha <aisha.developer.20@gmail.com>
2023-11-16 14:00:59 +0500

    Initial Commit

---
+++ Models/Contact.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+const mongodbURI = process.env.mongodbURI || "mongodb+srv://galaxysolutionsincorporated:galaxysolutionsincorporated@cluster0.tlxtt8v.mongodb.net/";
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    phone: String,
+    subject: String,
+    message: String
+  });
+ export const Contact = mongoose.model('Contact', contactSchema);
+  
+ const tripSchema = new mongoose.Schema({
+    name: String,
+    people: Number,
+    checkinDate: Date,
+    checkoutDate: Date
+  });
+  
+ export const Trip = mongoose.model('Trip', tripSchema);  
+
+mongoose.connect(mongodbURI);
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+mongoose.connection.on('connected', function () {//connected
+    console.log("Mongoose is connected");
+});
+
+mongoose.connection.on('disconnected', function () {//disconnected
+    console.log("Mongoose is disconnected");
+    process.exit(1);
+});
+
+mongoose.connection.on('error', function (err) {//any error
+    console.log('Mongoose connection error: ', err);
+    process.exit(1);
+});
+
+process.on('SIGINT', function () {/////this function will run jst before app is closing
+    console.log("app is terminating");
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection closed');
+        process.exit(0);
+    });
+});
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
---
+++ index.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import cors from 'cors'
+import { Contact } from './Models/Contact.js';
+import { Trip } from './Models/Contact.js';
+const app = express();
+const port = 8000;
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["https://worldwidetours.co", "*"],
+    credentials: true,
+  })
+);
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+app.post('/api/v1/contact', async (req, res) => {
+  try {
+    const contactData = req.body;
+    console.log(contactData)
+    const newContact = new Contact(contactData);
+    await newContact.save();
+    res.status(201).json(newContact);
+  } catch (error) {
+    res.status(500).json({ error: 'Could not create contact' });
+  }
+});
+
+
+app.post('/api/v1/trip', async (req, res) => {
+  try {
+    const tripData = req.body; 
+    const newTrip = new Trip(tripData);
+    await newTrip.save();
+    res.status(201).json(newTrip);
+  } catch (error) {
+    res.status(500).json({ error: 'Could not create trip' });
+  }
+});
+
+
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
